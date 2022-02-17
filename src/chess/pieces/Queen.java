7e792671dfe31f6fb19ecfab734e1722aae4b2c1
Guilherme Yuji Koyama Soken <guilhermesoken2001@gmail.com>
2022-02-17 12:35:33 -0300

    Rainha

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -24,7 +24,7 @@ public class Queen extends ChessPiece {
 		
 		// above
 		p.setValues(position.getRow() - 1, position.getColumn());
-		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+		while (getBoard().positionExists(p) && !getBoard().thereIsPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setRow(p.getRow() - 1);
 		}
