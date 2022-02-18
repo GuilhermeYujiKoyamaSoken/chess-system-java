674d98318c34ce883bb736b758ef96539166af64
Guilherme Yuji Koyama Soken <guilhermesoken2001@gmail.com>
2022-02-17 22:10:30 -0300

    Jogada especial de promoção

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -22,7 +22,7 @@ public class Rook extends ChessPiece {
 		
 		Position p = new Position(0, 0);
 		p.setValues(position.getRow() - 1, position.getColumn());
-		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+		while (getBoard().positionExists(p) && !getBoard().thereIsPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setRow(p.getRow() - 1);
 		}
@@ -32,7 +32,7 @@ public class Rook extends ChessPiece {
 		
 		// left
 		p.setValues(position.getRow(), position.getColumn() - 1);
-		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+		while (getBoard().positionExists(p) && !getBoard().thereIsPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setColumn(p.getColumn() - 1);
 		}
@@ -42,7 +42,7 @@ public class Rook extends ChessPiece {
 		
 		// right
 		p.setValues(position.getRow(), position.getColumn() + 1);
-		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+		while (getBoard().positionExists(p) && !getBoard().thereIsPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setColumn(p.getColumn() + 1);
 		}
